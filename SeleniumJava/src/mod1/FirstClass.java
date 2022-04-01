1e05370c15f07542aa4dd814eae0c3570061d233
Blessy Precilla <blessy.precilla@accenture.com>
2022-04-01 12:56:12 +0530

    First commit - navigate to google testcase

---
+++ SeleniumJava/src/mod1/FirstClass.java
@@ -0,0 +1,26 @@
+package mod1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\blessy.precilla\\Downloads\\chromedriver_win32\\chromedriver.exe" );
+	WebDriver driver = new ChromeDriver();
+	driver.get("https://www.google.com");
+	driver.manage().window().maximize();
+	String at = driver.getTitle();
+	String et = "Google";
+	driver.close();
+if(at.equalsIgnoreCase(et))
+{
+	System.out.println("Test Successfull");
+}
+else
+{
+	System.out.println("Test Failure");
+}
+	}
+
+}
